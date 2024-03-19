8b97f828b5162ae0a0ff759693cf6f3d431bff80
Jessie-Chuks <jessiechuksaginam@gmail.com>
2024-03-19 11:37:28 -0800

    Feat: Integrated smart contract into the frontend app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
 
 const getEthereumObject = () => window.ethereum;
 
@@ -30,6 +32,8 @@ const findMetaMaskAccount = async () => {
 };
 
 function App() {
+  const contractAddress = "0x6af63e28726404654059407773593fba5AC39aF8";
+  const contractABI = abi.abi;
   const [currentAccount, setCurrentAccount] = useState("");
 
   const connectWallet = async () => {
@@ -50,6 +54,35 @@ function App() {
       console.error(error);
     }
   };
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = await provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", Number(count));
+
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", Number(count));
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
     findMetaMaskAccount().then((account) => {
@@ -74,6 +107,7 @@ function App() {
           <label htmlFor="feedbackMessage">Feedback Message:</label>
           <textarea id="feedback" name="feedbackMessage" rows="4" required></textarea>
         </div>
+        <div onClick={wave}>wave</div>
         <button type="submit" className="submit-btn">Submit Feedback</button>
         <div className='feedbacks' id='feedbacks'></div>
       </form>
